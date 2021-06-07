5a009147a0f1bab53f1129c816f4fe27494e09e3
Glitch (storm-tangy-pest) <none>
2021-06-07 06:13:25 +0000

    🍃🕶 Checkpoint ./commands/help.js:31800669/5379

--- commands/help.js
+++ commands/help.js
@@ -1,23 +1,27 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
-  const commands = `connect\`\` - join the voice channel you are in
-   disconnect\`\` - leave the voice channel you are in
-   play <Song Name or url>\`\` - play songs from youtube
-   pause\`\` - pause currently playing songs in the server
-   resume\`\` - resume paused songs in the server
-   queue\`\` - shows the song queue of the server
-   skip\`\` - skips to next song in the queue
-   skipto <Target number>\`\` - Multiple skips until target
-   stop\`\` - stops the song and clears the queue
-   volume <volume count or none>\`\` - see or adjust volume of songs
-   np\`\` - see now playing song
-   lyrics\`\` - get lyrics of current song
-   shuffle\`\` - shuffle and randomize the queue
-   invite\`\` - get invite link for the bot
-   loop\`\` - enable / disable loop for the currently playing song
-   remove <Target number>\`\` - remove a song from the queue
-   help\`\` - to see this command`;
+  const commands = `**Commands :**
+
+• PB!connect - join the voice channel you are in
+• PB!disconnect - leave the voice channel you are in
+• PB!play <Song Name or url> - play songs from youtube
+• PB!pause - pause currently playing songs in the server
+• PB!resume - resume paused songs in the server
+• PB!queue - shows the song queue of the server
+• PB!skip - skips to next song in the queue
+• PB!skipto <Target number> - Multiple skips until target
+• PB!stop - stops the song and clears the queue
+• PB!volume <volume count or none> - see or adjust volume of songs
+• PB!np - see now playing song
+• PB!lyrics - get lyrics of current song
+• PB!shuffle - shuffle and randomize the queue
+• PB!invite - get invite link for the bot
+• PB!loop - enable / disable loop for the currently playing song
+• PB!remove <Target number> - remove a song from the queue
+• PB!help - to see this command
+
+`;
 
   const revised = commands
     .split("\n")
