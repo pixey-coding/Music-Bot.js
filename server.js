5a7efa303ec97cd6477a1dc64024d73420ae2a5a
Glitch (storm-tangy-pest) <none>
2021-06-07 05:56:38 +0000

    👰🔔 Checkpoint ./server.js:31800669/404

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log("Ping received!");
+  response.sendStatus(200);
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+and then in bot.js include this line:
+const server = require('./server.js');
