a1a4984108fca87d16f9b6387d9297b012229cb9
Glitch (regal-forest-mole) <none>
2021-06-08 05:52:31 +0000

    🏖📌 Checkpoint ./server.js:31800669/32

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  console.log("Ping received!");
+  console.log("Got Ping");
   response.sendStatus(200);
 });
 
